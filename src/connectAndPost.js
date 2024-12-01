293d00fe8a36cad54e55389d3286926a93094086
Justin Elliott <thecrashtestjustin@gmail.com>
2024-12-01 08:43:34 -0800

    updating the post text content

--- src/connectAndPost.js
+++ src/connectAndPost.js
@@ -33,17 +33,17 @@ export const run = async () => {
 
   let text;
   if (daysUntilXmas === 0) {
-    text = `Merry Christmas!🎄 ${catchPhrase}`;
+    text = `Merry Christmas!🎄🎉🎅🥂 ${catchPhrase}`;
   } else if (daysUntilXmas === 1) {
     text = `Only ${daysUntilXmas} day until Christmas!🎄 ${catchPhrase}`;
   } else if (daysUntilXmas <= 7) {
-    text = `There's only ${daysUntilXmas} days until Christmas 👀🥳🎄! ${catchPhrase}`;
+    text = `There's only ${daysUntilXmas} days until Christmas! 👀🎄 ${catchPhrase}`;
   } else if (daysUntilXmas > 60) {
-    text = `There are ${daysUntilXmas} days until Christmas Day🎄. ${catchPhrase}`;
+    text = `There are ${daysUntilXmas} days until Christmas Day!🎄 ${catchPhrase}`;
   } else {
     const weeksUntikXmas = (daysUntilXmas / 7).toFixed(1);
     const hoursUntilXmas = daysUntilXmas * 24;
-    text = `There are only ${daysUntilXmas} days until Christmas day 🥳🎄. That's ${weeksUntikXmas} weeks or ${hoursUntilXmas} hours! ${catchPhrase}`;
+    text = `There are only ${daysUntilXmas} days until Christmas day! 🎉🎄 That's ${weeksUntikXmas} weeks or ${hoursUntilXmas} hours! ${catchPhrase}`;
   }
 
   await agent.post({
